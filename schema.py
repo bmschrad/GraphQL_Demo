10329a6b7a2287973398c020084102aa6c5ebc31
Brandon Schrader <brandon@simpleltc.com>
2019-12-10 13:31:07 -0600

    Removed test structure

--- schema.py
+++ schema.py
@@ -1,35 +1,6 @@
 from graphene import ObjectType, String, Boolean, ID, Field, Int, List
-from resource import review_json, providers
-from json2obj import json2obj
+from resource import providers
 import json
-
-class User(ObjectType):
-    first_name = String()
-    has_profile_pic = Boolean()
-    id = ID()
-    picture_url = String()
-    smart_name = String()
-    thumbnail_url = String()
-
-class Listing(ObjectType):
-    id = ID()
-    name = String()
-
-class Review(ObjectType):
-    author = Field(User)
-    author_id = ID()
-    can_be_edited = Boolean()
-    comments = String()
-    created_at = String()
-    id = Int()
-    language = String()
-    listing = Field(Listing)
-    listing_id = ID()
-    recipient = Field(User)
-    recipient_id = ID()
-    response = String()
-    role = String()
-    user_flag = Boolean()
 
 class Providers(ObjectType):
     uid = Int()
@@ -45,15 +16,6 @@ class Query(ObjectType):
     reviews = List(Review)
     providers = List(Providers)
     hello = String(description="Hello")
-
-    def resolve_reviews(context, info):
-                # reviews = api_call(args.get("id"))["reviews"]
-                # breakpoint()
-                reviews = review_json['reviews'][-1]
-                print(reviews)
-                # return json2obj(json.dumps(reviews))
-                # return json2obj(reviews)
-                return json.dumps(reviews)
 
     def resolve_providers(self, info, **args):
         print(providers)
