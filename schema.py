330d5580a2cc7f0d86c803306cc51c2e6015a7d6
Brandon Schrader <brandon@simpleltc.com>
2019-12-10 19:33:50 -0600

    Added linking and rest of model to GraphQL

--- schema.py
+++ schema.py
@@ -1,6 +1,34 @@
 from graphene import ObjectType, String, Boolean, ID, Field, Int, List
-from resource import providers
+from resource import providers, provider_products, provider_users, users_log
 import json
+
+class Products(ObjectType):
+    uid = Int()
+    provider_uid = Int()
+    product_name = String()
+    start_of_service = String()
+    end_of_service = String()
+    added_on = String()
+
+
+class Logs(ObjectType):
+    uid = Int()
+    user_uid = Int()
+    login_time = String()
+
+
+class Users(ObjectType):
+    uid = Int()
+    provider_uid = Int()
+    username = String()
+    first_name = String()
+    last_name = String()
+    email = String()
+    logs = List(Logs)
+
+    def resolve_logs(self, info, **args):
+        return list(filter(lambda x: x['user_uid'] == self['uid'], users_log))
+
 
 class Providers(ObjectType):
     uid = Int()
@@ -11,6 +39,15 @@ class Providers(ObjectType):
     city = String()
     state = String()
     zip = String()
+    products = List(Products)
+    users = List(Users)
+
+    def resolve_products(self, info, **args):
+        return list(filter(lambda x: x['provider_uid'] == self['uid'], provider_products))
+
+    def resolve_users(self, info, **args):
+        return list(filter(lambda x: x['provider_uid'] == self['uid'], provider_users))
+
 
 class Query(ObjectType):
     providers = List(Providers, uid=Int(required=False))
